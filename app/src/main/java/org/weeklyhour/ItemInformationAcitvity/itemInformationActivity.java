801e9f2da84b161b3bb64137c45ae17aca0006dc
gwlee <vvvb78@gmail.com>
2016-09-25 11:59:52 +0900

    2016-09-25 ItemInformationActivity ...
    - 임시 push
    
    TODO
    // Start Thread 구현
    // Custom Activity (조그만걸로) Detail 페이지 편집

--- app/src/main/java/org/weeklyhour/ItemInformationAcitvity/itemInformationActivity.java
+++ app/src/main/java/org/weeklyhour/ItemInformationAcitvity/itemInformationActivity.java
@@ -2,15 +2,18 @@ package org.weeklyhour.ItemInformationAcitvity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import org.weeklyhour.MainActivity.R;
 
 public class itemInformationActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_information_item);
     }
+
+
 }
